ec4f55f059cf908310faf31c729233b7878a5731
CodingMie <544286175@qq.com>
2026-01-17 23:33:32 +0800

    update

--- .next/dev/static/chunks/src_app_game_page_tsx_70623a41._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_c3ed5dd8._.js",
-  "static/chunks/node_modules_lucide-react_dist_esm_86223a28._.js"
-],
-    source: "dynamic"
-});
